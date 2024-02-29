77493ed15a3179816b2c1f5c5837720780ed3700
jrns1 <jrns11@snu.ac.kr>
2024-02-29 17:17:52 +0900

    feat: fileter by published

--- blog/views.py
+++ blog/views.py
@@ -14,11 +14,19 @@ class PostListAPIView(generics.ListAPIView):
     
     def get_queryset(self):
         address = self.kwargs.get('address')
-        if (User.objects.get(address=address) is None):
+
+        author = User.objects.get(address=address)
+        if (author is None):
             return Response(status=status.HTTP_404_NOT_FOUND)
         
         queryset = super().get_queryset().filter(author__address=address)
+
+        # filter by published 
+        user = self.request.user
+        if (user.is_anonymous or user.address != author.address):
+            queryset = queryset.filter(published=True)
         
+        # filter by year and month
         year = self.request.query_params.get('year')
         month = self.request.query_params.get('month')
         if year is not None and month is not None:
@@ -30,8 +38,6 @@ class PostListAPIView(generics.ListAPIView):
             except ValueError:
                 return Response(status=status.HTTP_400_BAD_REQUEST)
 
-        ### to add: filter depending on published status
-            
         return queryset.order_by('date')
 
 
