dcd45844f3e3b4b6c75fc9611755c40e87661c2a
jrns1 <jrns11@snu.ac.kr>
2024-04-25 16:07:11 +0900

    feat: Refresh/Blacklist request시 토큰을 body에서

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 import accounts.views as views
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenRefreshView, TokenBlacklistView
 
 urlpatterns = [
     path(r'kakao/login/complete/', views.KakaoSocialLogin.as_view(), name='kakao_login_complete'),
-    path(r'token/refresh/', views.CookieTokenRefreshView.as_view(), name='token_refresh'),
-    path(r'logout/', views.CookieTokenBlacklistView.as_view(), name='logout'),
+    path(r'token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path(r'logout/', TokenBlacklistView.as_view(), name='logout'),
     path(r'me/', views.UserRetrieveUpdateAPIView.as_view(), name='me'),
     path(r'profile/<str:address>/', views.UserRetrieveAPIView.as_view(), name='profile'),
 ]
