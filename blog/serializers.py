c7501ef6fd6e339ca14c83b799794948cb21598f
jrns1 <jrns11@snu.ac.kr>
2024-04-25 18:48:04 +0900

    feat: 중복 date에 대한 CREATE 요청 거부

--- blog/serializers.py
+++ blog/serializers.py
@@ -15,6 +15,12 @@ class PostCreateSerializer(serializers.ModelSerializer):
         fields = '__all__'
         read_only_fields = ['author']
 
+    def validate_date(self, value):
+        author = self.context['request'].user
+        if Post.objects.filter(date=value, author=author).exists():
+            raise serializers.ValidationError('This date is already taken')
+        return value
+
     def create(self, validated_data):
         address = self.context['request'].data['address']
         validated_data['author'] = User.objects.get(address=address)
