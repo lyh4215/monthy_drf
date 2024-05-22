0ff5d1b5e067006044e757547e42b2c669e89556
jrns1 <jrns11@snu.ac.kr>
2024-05-22 18:50:34 +0900

    fix: last commit

--- accounts/signals.py
+++ accounts/signals.py
@@ -5,7 +5,9 @@ from blog.models import Post
 from datetime import datetime
 
 TUTORIAL_THUMB = 'Have a nice month!'
-TUTORIAL_BODY = '{"type":"doc","content":[{"type":"paragraph","content":[{"type":"text","text":"간단한 사용법"}]},{"type":"orderedList","attrs":{"start":1},"content":[{"type":"listItem","content":[{"type":"paragraph","content":[{"type":"text","text":"달력에 넣을 사진에 별표"}]}]},{"type":"listItem","content":[{"type":"paragraph","content":[{"type":"text","text":"문장은 꾸욱 드래그해서 별표"}]}]},{"type":"listItem","content":[{"type":"paragraph","content":[{"type":"text","text":"공개한 일기는 링크를 알려준 친구만 볼 수 있어요"}]}]}]},{"type":"paragraph"},{"type":"paragraph"},{"type":"paragraph","content":[{"type":"text","marks":[{"type":"thumb"}],"text":"Have a nice month!"}]},{"type":"paragraph"}]}'
+TUTORIAL_BODY = '''
+{"type":"doc","content":[{"type":"paragraph","content":[{"type":"text","text":"간단한 사용법"}]},{"type":"orderedList","attrs":{"start":1},"content":[{"type":"listItem","content":[{"type":"paragraph","content":[{"type":"text","text":"달력에 넣을 사진에 별표"}]}]},{"type":"listItem","content":[{"type":"paragraph","content":[{"type":"text","text":"문장은 꾸욱 드래그해서 별표"}]}]},{"type":"listItem","content":[{"type":"paragraph","content":[{"type":"text","text":"공개한 일기는 링크를 알려준 친구만 볼 수 있어요"}]}]}]},{"type":"paragraph"},{"type":"paragraph"},{"type":"paragraph","content":[{"type":"text","marks":[{"type":"thumb"}],"text":"Have a nice month!"}]},{"type":"paragraph"}]}
+'''
 
 @receiver(post_save, sender=User)
 def set_username_from_email(sender, instance, created, **kwargs):
