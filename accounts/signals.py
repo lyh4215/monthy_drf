7eff3dcaabb4df14cbba8a4372ef382324c86b17
jrns1 <jrns11@snu.ac.kr>
2024-06-13 07:04:44 +0900

    fix: 튜토리얼 내용 제목 통일

--- accounts/signals.py
+++ accounts/signals.py
@@ -4,7 +4,7 @@ from accounts.models import User
 from blog.models import Post
 from datetime import datetime
 
-TUTORIAL_THUMB = 'Have a nice month!'
+TUTORIAL_THUMB = '마음에 드는 문장'
 TUTORIAL_BODY = '''
 {"type":"doc","content":[{"type":"heading","attrs":{"level":2},"content":[{"type":"text","text":"How to monthy?"}]},{"type":"paragraph","content":[{"type":"text","text":"클릭해서 바로 수정할 수 있어요. "}]},{"type":"paragraph","content":[{"type":"text","text":"monthy의 핵심은 "},{"type":"text","marks":[{"type":"bold"}],"text":"별표"},{"type":"text","text":" 버튼! ⭐️ (수정할 때만 보여요)"}]},{"type":"paragraph","content":[{"type":"text","text":"이렇게 "},{"type":"text","marks":[{"type":"thumb"}],"text":"마음에 드는 문장"},{"type":"text","text":"을 "},{"type":"text","marks":[{"type":"bold"}],"text":"드래그"},{"type":"text","text":"해서 "},{"type":"text","marks":[{"type":"underline"}],"text":"제목"},{"type":"text","text":"을 지정할 수도 있구요, 오늘의 "},{"type":"text","marks":[{"type":"underline"}],"text":"대표 사진"},{"type":"text","text":"을 설정할 수도 있어요!"}]},{"type":"paragraph"},{"type":"paragraph"},{"type":"paragraph"},{"type":"paragraph","content":[{"type":"text","text":"     추신. 달력 위 배너 사진을 넣을 수 있어요."}]}]}
 '''
