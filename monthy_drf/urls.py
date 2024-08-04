6634e4ed83065ac6531cc66a8389667ad9484bf3
lyh4215 <lyh4215@naver.com>
2024-08-04 10:35:31 +0900

    fix:kakao social url error fix

--- monthy_drf/urls.py
+++ monthy_drf/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('accounts/', include('accounts.urls')),
+    path('accounts/', include('allauth.urls'), name='socialaccount_signup')
 ]
 # ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
