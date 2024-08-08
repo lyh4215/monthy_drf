23cbe1e40c854cc013abb021870032c688dfaf04
lyh4215 <lyh4215@naver.com>
2024-08-08 13:13:08 +0900

    fix: post image create validation error

--- blog/serializers.py
+++ blog/serializers.py
@@ -43,17 +43,27 @@ class PostImageCreateSerializer(serializers.ModelSerializer):
         fields = ['src', 'post', 'name_hash', 'device_id']
         read_only_fields = ['post', 'name_hash', 'device_id']
 
-    def validate_date(self, value):
+    def validate(self, data):
+        date = self.context.get('date')
+        name_hash = self.context.get('name_hash')
+        device_id = self.context.get('device_id')
         author = self.context['request'].user
-        if not Post.objects.filter(date=value, author=author).exists():
-            #TODO : add template (prepare about not expected disconnect)
-            Post.objects.create(date=value, author=author)
-        return value
 
-    def validate_device_id(self, value):
-        if not isinstance(value, uuid.UUID):
-            raise serializers.ValidationError("Invalid UUID")
-        return value
+        if not Post.objects.filter(date=date, author=author).exists():
+            #TODO : add template (prepare about not expected disconnect)
+            Post.objects.create(date=date, author=author)
+
+        try:
+            device_id = uuid.UUID(device_id)
+        except ValueError:
+            raise serializers.ValidationError({"device_id": "Invalid UUID format"})
+        self.context['device_id'] = device_id
+        
+        if not name_hash:
+            raise serializers.ValidationError({"name_hash": "Name hash is required"})
+        
+        return data
+    
 
     def create(self, validated_data):
         date = self.context['date']
