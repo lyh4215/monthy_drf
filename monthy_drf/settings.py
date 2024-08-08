0101e487e31d3807da4896e4aff12a61047fa3b8
lyh4215 <lyh4215@naver.com>
2024-08-08 09:51:08 +0900

    feat: change authorization hearer key

--- monthy_drf/settings.py
+++ monthy_drf/settings.py
@@ -84,7 +84,7 @@ REST_FRAMEWORK = {
     ),
       'DEFAULT_AUTHENTICATION_CLASSES': (
         # 'rest_framework.authentication.SessionAuthentication',
-        'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
+        'accounts.authorization.JWTXAuthentication',
     ),
 }
 
