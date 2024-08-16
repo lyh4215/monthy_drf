0efbbb819b470a4d725c5e384ad15769499451c9
lyh4215 <lyh4215@naver.com>
2024-08-16 23:27:04 +0900

    docs: add annotation line in llm

--- nudge/llm/__init__.py
+++ nudge/llm/__init__.py
@@ -7,6 +7,7 @@ import os
 
 # OpenAI 객체를 llm으로 설정
 llm = ChatOpenAI(api_key=os.getenv('OPENAI_API_KEY'), model='gpt-3.5-turbo')
+# llm = ChatOpenAI(api_key=os.getenv('OPENAI_API_KEY'), model='gpt-4o')
 tool = TavilySearchResults(max_results=2)
 tools = [tool]
 llm_with_tools = llm.bind_tools(tools)
