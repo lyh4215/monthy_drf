621ff0f7d47f53b912e5f97bad2cb0e7b033a36a
lyh4215 <lyh4215@naver.com>
2024-08-28 17:19:22 +0900

    feat: add gcp logging

--- monthy_drf/settings/prod.py
+++ monthy_drf/settings/prod.py
@@ -1,4 +1,8 @@
 from .base import *
+import google.cloud.logging
+from google.cloud.logging.handlers import CloudLoggingHandler, setup_logging
+client = google.cloud.logging.Client()
+cloud_handler = CloudLoggingHandler(client)
 
 ALLOWED_HOSTS = ['api.monthy-api.com', '34.64.209.29', '.pythonanywhere.com']
 
@@ -11,12 +15,19 @@ LOGGING = {
             'class': 'logging.FileHandler',
             'filename': '/var/log/django/error.log',
         },
+        'cloud': {
+               'level': 'DEBUG',
+               'class': 'google.cloud.logging.handlers.CloudLoggingHandler',
+               'client': client,
+        },
     },
     'loggers': {
         'django': {
-            'handlers': ['file'],
-            'level': 'ERROR',
+            'handlers': ['cloud', 'console'],
+            'level': 'DEBUG',
             'propagate': True,
         },
     },
 }
+
+setup_logging(cloud_handler)
