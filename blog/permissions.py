0e2008cd4471eb8ffe940dae0d3f63657b30c7c3
lyh4215 <lyh4215@naver.com>
2024-08-28 17:51:04 +0900

    fix: change IsAuthor permission

--- blog/permissions.py
+++ blog/permissions.py
@@ -6,5 +6,8 @@ class IsAuthor(permissions.BasePermission):
     Assumes the model instance has an `owner` attribute.
     """
 
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
+
     def has_object_permission(self, request, view, obj):
         return obj.author == request.user
