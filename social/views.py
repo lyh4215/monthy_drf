a0aacd7ecfdd07ef59cb780eb89e1032211df993
lyh4215 <lyh4215@naver.com>
2024-09-01 05:51:43 +0900

    refactor: clarity of allowed method

--- social/views.py
+++ social/views.py
@@ -1,18 +1,20 @@
-from django.shortcuts import render
 from rest_framework import viewsets
-from rest_framework.exceptions import MethodNotAllowed, ValidationError
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.authentication import TokenAuthentication
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import mixins
 from .models import Friend
-from .serializers import FriendSerializer, FriendSendSerializer, FriendReceiveSerializer
+from .serializers import FriendSendSerializer, FriendReceiveSerializer
 from .permissions import IsFriendSender, IsFriendReceiver
 from django.contrib.auth import get_user_model
 
+
 User = get_user_model()
 
-class FriendSendViewSet(viewsets.ModelViewSet):
+class FriendSendViewSet(mixins.CreateModelMixin,
+                        mixins.ListModelMixin,
+                        mixins.RetrieveModelMixin,
+                        mixins.DestroyModelMixin,
+                        viewsets.GenericViewSet):
     serializer_class = FriendSendSerializer
     permission_classes = [IsFriendSender]
     lookup_field = 'friend__address'
@@ -23,20 +25,17 @@ class FriendSendViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
     
-    def update(self, request, *args, **kwargs):
-        raise MethodNotAllowed("Update")
-    
-    
-class FriendReceiveViewSet(viewsets.ModelViewSet):
+class FriendReceiveViewSet(mixins.UpdateModelMixin,
+                           mixins.ListModelMixin,
+                           mixins.RetrieveModelMixin,
+                           mixins.DestroyModelMixin,
+                           viewsets.GenericViewSet):
     permission_classes = [IsFriendReceiver]
     lookup_field = 'user__address'
     serializer_class = FriendReceiveSerializer
     def get_queryset(self):
         user = self.request.user
         return Friend.objects.filter(friend=user)
-    
-    def create(self, request, *args, **kwargs):
-        raise MethodNotAllowed("Create")
     
     def update(self, request, *args, **kwargs):
         friend_request = self.get_object()
@@ -45,7 +44,7 @@ class FriendReceiveViewSet(viewsets.ModelViewSet):
             friend_request.save()
             return Response({'status': 'accepted'}, status=status.HTTP_200_OK)
         elif friend_request.status == 'accepted':
-            return Response({'status': 'already accepted'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'status': 'already accepted'}, status=status.HTTP_200_OK)
         else:
             return Response({'status': 'error'}, status=status.HTTP_400_BAD_REQUEST)
     
