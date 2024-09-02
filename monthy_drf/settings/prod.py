251a2b940bbab3633710f92a16df6e26aecd93f8
lyh4215 <lyh4215@naver.com>
2024-09-03 06:55:02 +0900

    fix: add middleware settings

--- monthy_drf/settings/prod.py
+++ monthy_drf/settings/prod.py
@@ -35,4 +35,15 @@ LOGGING = {
     },
 }
 
-setup_logging(cloud_handler)
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
+    'monthy_drf.middleware.RequestLoggingMiddleware',
+]
